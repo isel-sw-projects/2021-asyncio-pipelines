7729e18e154586ae69d0f44c9a28da89af11abda
unknown <dporodrigues@sapo.pt>
2022-01-10 00:59:23 +0000

    Continuation of implementation of charpter 2.3

---
+++ latex_write_project/Chapters/scripts/asyncenum.js
@@ -0,0 +1,29 @@
+async function* streamAsyncIterable(stream) {
+    const reader = stream.getReader();
+    try {
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) {
+          return;
+        }
+        yield value;
+      }
+    } finally {
+      reader.releaseLock();
+    }
+  }
+
+async function getResponseSize(url) {
+    const response = await fetch(url);
+    // Will hold the size of the response, in bytes.
+    let responseSize = 0;
+    
+    const iterable = streamAsyncIterable(response.body);
+    
+    // The for-await-of loop. Async iterates over each portion of the response.
+    for await (const chunk of iterable) {
+      // Incrementing the total response length.
+      responseSize += chunk.length;
+    }
+    return responseSize;
+}  
