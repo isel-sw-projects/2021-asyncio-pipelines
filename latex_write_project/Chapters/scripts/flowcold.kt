662e0a6d74195cd2c30187b040c339307bfb6432
Diogo Rodrigues <diogopaulo.rodrigues@edp.com>
2022-02-10 17:55:57 +0000

    Kotlin subchapter development

---
+++ latex_write_project/Chapters/scripts/flowcold.kt
@@ -0,0 +1,29 @@
+fun simple(): Flow<Int> = flow { 
+    println("Flow started")
+    for (i in 1..3) {
+        delay(100)
+        emit(i)
+    }
+}
+
+fun main() = runBlocking<Unit> {
+    println("Calling simple function...")
+    val flow = simple()
+    println("Calling collect...")
+    flow.collect { value -> println(value) } 
+    println("Calling collect again...")
+    flow.collect { value -> println(value) } 
+}
+
+
+Calling simple function...
+Calling collect...
+Flow started
+1
+2
+3
+Calling collect again...
+Flow started
+1
+2
+3
