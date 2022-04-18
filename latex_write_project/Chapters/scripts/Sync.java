f6ed5ea1f6b5d787c5286725aa7adaa4d5751c8d
unknown <dporodrigues@sapo.pt>
2022-04-18 20:02:28 +0100

    Fixed several issues with CAD report

--- latex_write_project/Chapters/scripts/Sync.java
+++ latex_write_project/Chapters/scripts/Sync.java
@@ -1,12 +1,11 @@
-// create a client
+
 HttpClient client = HttpClient.newHttpClient();
 
-// create a request
 HttpRequest request = HttpRequest.newBuilder(
-
-         URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
+         URI.create("SOME URL"))
          .header("accept", "application/json")
          .build();
 
-HttpResponse<Supplier<DTO>> response = client.send(request, new JsonBodyHandler<>(DTO.class));
+HttpResponse<Supplier<DTO>> response = 
+        client.send(request, new JsonBodyHandler<>(DTO.class));//blocks
 
--- latex_write_project/Chapters/scripts/async.java
+++ latex_write_project/Chapters/scripts/async.java
@@ -2,12 +2,12 @@
 HttpClient client = HttpClient.newHttpClient();
 
 HttpRequest request = HttpRequest.newBuilder(
-        URI.create("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"))
+        URI.create("SOME URL"))
                 .header("accept", "application/json")
                 .build();
 
 var futureResponse = client
         .sendAsync(request, new JsonBodyHandler<>(DTO.class)) 
-        .thenAccept(res -> System.out.println(res.body().get().title)); //consumer callback for when the operation finishes
+        .thenAccept(res -> res.body().get().title); 
 
-futureResponse.get();
+Console.WriteLine("prints something")
